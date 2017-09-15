9754833e75b69c5c566375865188de2da77022f4
AEmpire <18751294896@163.com>
2017-09-15 15:52:10 +0800

    添加项目文件。

---
+++ UdpBroadcastTest/Program.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpBroadcastTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Sender");
+// This constructor arbitrarily assigns the local port number.
+
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.Connect("192.168.1.126", 11000);
+            try
+            {
+                string message = String.Empty;
+                do
+                {
+                    message = Console.ReadLine();
+// Sends a message to the host to which you have connected.
+                    Byte[] sendBytes = Encoding.ASCII.GetBytes(message);
+
+                    udpClient.Send(sendBytes, sendBytes.Length);
+                } while (message != String.Empty);
+
+                udpClient.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Press Any Key to Continue");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ UdpReceiver/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace UdpReciever
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Receiver");
+// This constructor arbitrarily assigns the local port number.
+            UdpClient udpClient = new UdpClient();
+            udpClient.Client.SetSocketOption(SocketOptionLevel.Socket, SocketOptionName.ReuseAddress, true);
+            udpClient.Client.Bind(new IPEndPoint(IPAddress.Parse("192.168.1.126"), 11000));
+            try
+            {
+//IPEndPoint object will allow us to read datagrams sent from any source.
+                IPEndPoint RemoteIpEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+                string message = String.Empty;
+                do
+                {
+
+// Blocks until a message returns on this socket from a remote host.
+                    Byte[] receiveBytes = udpClient.Receive(ref RemoteIpEndPoint);
+                    message = Encoding.ASCII.GetString(receiveBytes);
+
+// Uses the IPEndPoint object to determine which of these two hosts responded.
+                    Console.WriteLine("This is the message you received: " +
+                                      message);
+//Console.WriteLine("This message was sent from " +
+//                            RemoteIpEndPoint.Address.ToString() +
+//                            " on their port number " +
+//                            RemoteIpEndPoint.Port.ToString());
+                } while (message != "exit");
+                udpClient.Close();
+//udpClientB.Close();
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.ToString());
+            }
+
+            Console.WriteLine("Press Any Key to Continue");
+            Console.ReadKey();
+        }
+    }
+}
